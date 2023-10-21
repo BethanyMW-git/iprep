48510d66c4f212ac9cc1ea00c50f2a31941b71bf
Bethany White <bethanymwhite@gmail.com>
2023-10-20 20:56:39 -0400

    added Checkout page starter code

---
+++ src/Checkout.js
@@ -0,0 +1,65 @@
+//import App from "./App";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-boostrap/Form";
+import Row from "react-bootstrap/Row";
+
+
+export default function Checkout() {
+    return (
+        <div id="checkout-box">
+            <Container>
+                <Row id="quiz-results">
+                    <Col>
+                        <h3>Quiz Results</h3>
+                        <ul>
+                            <li>Omnivore</li>
+                            <li>2 person household</li>
+                            <li>Chinese cuisine</li>
+                            <li>nut allergy</li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row id="suggested-product">
+                    <Col>
+                        <p>Our Silver package is the best fit for your needs:</p>
+                    </Col>
+                </Row>
+                <Row id="product-offerings">
+                    <Col>
+                        {/* Bronze
+                        <input type="radio">$50/wk</input> */}
+                        <Form>
+                            <Form.Label>Bronze $50</Form.Label>
+                            <Form.Check type="radio"></Form.Check>
+                        </Form>
+                    </Col>
+                    <Col>
+                        {/* Silver
+                        <input type="radio">$100/wk</input> */}
+                        <Form>
+                            <Form.Label>Silver $100/wk</Form.Label>
+                            <Form.Check type="radio"></Form.Check>
+                        </Form>
+                    </Col>
+                    <Col>
+                        {/* Gold
+                        <input type="radio">$150/wk</input> */}
+                        <Form>
+                            <Form.Label>Gold $150/wk</Form.Label>
+                            <Form.Check type="radio"></Form.Check>
+                        </Form>
+                    </Col>
+                </Row>
+                <Row id="total">
+                    <p>Total: $100</p>
+                </Row>
+                <Row id="checkout-button">
+                    <Button>Checkout</Button>
+                </Row>
+            </Container>
+        </div>
+        
+    );
+};
